a6d86be807c40ec6284d08f994dc3db5515dd880
nescioquid <info@creachr.com>
2025-11-14 20:48:12 -0500

    added preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tutorial-astro-creachr.netlify.app',
+  integrations: [preact()],
 })
